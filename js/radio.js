aaeb98a542c7de0cd111fa71e32ec9a37c55ccee
operadorbrasil7233 <139518378+operadorbrasil7233@users.noreply.github.com>
2025-02-28 14:14:20 -0300

    Update radio.js

--- js/radio.js
+++ js/radio.js
@@ -8,13 +8,13 @@ function moura() {
         qual = 'Visitante'
     }
     if (hora >= 7 && hora < 12) {
-        res.innerHTML = `olá Bom dia,Seja Bem Vindo a nossa Webrádio Torre Forte, Jesus Te ama e certamente tem uma palavra e um louvor para o seu coração ${qual}`
+        res.innerHTML = `olá Bom dia,Seja Bem Vindo a nossa Webrádio Torre Forte, Jesus Te ama  ${qual}`
         img.src = './img/1000448259-removebg-preview (2).png'
     } else if (hora < 18) {
         img.src = './img/1000448259-removebg-preview (2).png';
-        res.innerHTML = `Olá Boa Tarde,Seja Bem Vindo a nossa rádio Torre Forte Jesus Te ama e certamente tem uma palavra e um louvor para o seu coração ${qual}`
+        res.innerHTML = `Olá Boa Tarde,Seja Bem Vindo a nossa rádio Torre Forte Jesus Te ama ${qual}`
     } else if (hora < 23) {
         img.src = './img/1000448259-removebg-preview (2).png'
-        res.innerHTML = `Olá Boa Noite,Seja Bem Vindo a nossa rádio Torre Forte Jesus Te Ama e certamente tem uma palavra e um louvor para o seu coração ${qual}`
+        res.innerHTML = `Olá Boa Noite,Seja Bem Vindo a nossa rádio Torre Forte Jesus Te Ama ${qual}`
     }
 }
