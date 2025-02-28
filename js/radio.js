12dade160b578d0790dede5855fc3c1597a44fa5
operadorbrasil7233 <139518378+operadorbrasil7233@users.noreply.github.com>
2025-02-28 09:24:36 -0300

    Update radio.js

--- js/radio.js
+++ js/radio.js
@@ -8,13 +8,13 @@ function moura() {
         qual = 'Visitante'
     }
     if (hora >= 7 && hora < 12) {
-        res.innerHTML = `Bom dia, ${qual}`
-        img.src = './img/manha.png'
+        res.innerHTML = `Bom dia,Seja Bem Vindo a nossa rádio Torre Forte Jesus Te ama ${qual}`
+        img.src = './img/Fundo de Tela para Celular Aquarela Mármore Minimalista Aesthetic Rosa e Br_20250228_091752_0000.png'
     } else if (hora < 18) {
-        img.src = '../img/tarde.gif';
-        res.innerHTML = `Boa Tarde, ${qual}`
+        img.src = '../img/Fundo de Tela para Celular Aquarela Mármore Minimalista Aesthetic Rosa e Br_20250228_091752_0000.png';
+        res.innerHTML = `Boa Tarde,Seja Bem Vindo a nossa rádio Torre Forte Jesus Te ama ${qual}`
     } else if (hora < 23) {
-        img.src = './img/triste.gif'
-        res.innerHTML = `Boa Noite, ${qual}`
+        img.src = './img/Fundo de Tela para Celular Aquarela Mármore Minimalista Aesthetic Rosa e Br_20250228_091752_0000.png'
+        res.innerHTML = `Boa Noite,Seja Bem Vindo a nossa rádio Torre Forte Jesus Te Ama ${qual}`
     }
 }
